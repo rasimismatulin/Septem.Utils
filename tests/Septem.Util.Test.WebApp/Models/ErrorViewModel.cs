6f599c54fc5edc5dbde5b4f93cb1727f38f37a4b
Rasim Ismatulin <rasim.ismatulin@gmail.com>
2022-04-19 12:51:19 +0400

    Removed folder from repository

--- tests/Septem.Util.Test.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Septem.Util.Test.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
