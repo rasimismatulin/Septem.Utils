0943e3bc724bf0d902c1e50ee903a0141e893b80
Rasim Ismatulin <rasim.ismatulin@gmail.com>
2022-11-23 19:25:39 +0400

    fixed dll loading

--- src/Septem.Utils.EntityFramework.Dynamic/tnp.cs
+++ src/Septem.Utils.EntityFramework.Dynamic/tnp.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Septem.Utils.EntityFramework.Dynamic
@@ -9,5 +13,49 @@ namespace Septem.Utils.EntityFramework.Dynamic
         [Key]
         [Column("test")]
         public string Name { get; set; }
+
+        public tnp()
+        {
+            
+        }
+    }
+
+
+    public class tnpDbContextDynamic : DbContext
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public DbSet<tnp> _1_5_table_eng { get; set; }
+
+
+        public tnpDbContextDynamic()
+        {
+        }
+
+        public tnpDbContextDynamic(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public tnpDbContextDynamic(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _loggerFactory = loggerFactory;
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+                return;
+
+            var connectionString = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
+        }
     }
 }
