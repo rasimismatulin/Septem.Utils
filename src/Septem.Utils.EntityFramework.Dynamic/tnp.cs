651fd703374ec7181aca992ca9a06841a6521507
Rasim Ismatulin <rasim.ismatulin@gmail.com>
2022-11-23 19:09:58 +0400

    fixed bug

---
+++ src/Septem.Utils.EntityFramework.Dynamic/tnp.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Septem.Utils.EntityFramework.Dynamic
+{
+    [Table("test")]
+    public class tnp
+    {
+        [Key]
+        [Column("test")]
+        public string Name { get; set; }
+    }
+}
